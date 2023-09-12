8e150c7536adea18035729206e42ed09eadb5950
Vittor fonseca serra <vittorserradev@gmail.com>
2023-09-12 16:48:02 -0300

    Removi a section destaque

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,12 +1,10 @@
 <script setup>
 document.title = 'Home'
 import Content from '../components/Content.vue';
-import Destaque from '../components/SliderDestaque.vue'
 </script>
 
 <template>
   <main>
-    <Destaque/>
     <Content/>
   </main>
 </template>
