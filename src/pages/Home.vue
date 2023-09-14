4aedc280a6de1acce7a24343f9f6be261adb892f
Vittor fonseca serra <vittorserradev@gmail.com>
2023-09-14 19:20:46 -0300

    New Card

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,7 +1,7 @@
 <script setup>
 document.title = "Home";
 import Anuncio from "../components/Anuncio.vue";
-import Content from "../components/Content.vue";
+//import Content from "../components/Content.vue";
 import Filter from "../components/Filter.vue";
 </script>
 
@@ -9,8 +9,55 @@ import Filter from "../components/Filter.vue";
   <main>
     <Anuncio />
     <Filter />
-    <Content />
+    <div class="container-card">
+      <div class="box-card">
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+      </div>
+    </div>
   </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+.container-card{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+}
+.box-card{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 5px;
+  height: auto;
+  width: 100%;
+}
+.card{
+  height: 350px; 
+  width: 220px;
+  background: black;
+  border-radius: 10px;
+}
+@media screen and (max-width:460px){
+  .card{
+    height: 450px;
+    width: 320px;
+  }
+}
+/*@media screen and (max-width:375px){
+  .card{
+    height: 450px;
+    width: 320px;
+  }
+}*/
+</style>
